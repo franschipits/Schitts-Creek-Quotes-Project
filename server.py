4e82baa0ca1361711f1031f870a11e6b15e3f67c
Franciane Schipits <franmifreitas@gmail.com>
2024-01-29 15:02:25 -0600

    initial

---
+++ server.py
@@ -0,0 +1,19 @@
+import random
+
+quotes = {"I like the wine, and not the label.-David Rose",
+          "I do not have a lot to my name right now, but I do have one thing: taste.-David Rose",
+          "I have never heard someone say so many wrong things, one after the other, consecutively, in a row.-David Rose",
+          "I could not be more at one with nature. I do Coachella every year.-David Rose",
+          "Congratulations on your ongoing love for one another. You did it.-David Rose",
+          "I’m starting to feel like I’m trapped in an Avril Lavigne lyric here.-David Rose",
+          "Excuse me, I haven’t bedazzled anything since I was 22.-David Rose",
+          "The internet is a breeding ground for freaks.-David Rose",
+          "I’m trying very hard not to connect with people right now.-David Rose",
+          "Like Beyonce, I excel as a solo artist and my mom dressed me well into my teens.-David Rose",
+          "Very uninterested in that opinion.-David Rose",
+          "I would hardly call myself an expert on this subject. And by subject, I mean genuine human emotion.-David Rose",
+          "I’ve been burned so many times I’m like the human equivalent of the inside of a roasted marshmallow.-David Rose",
+          }
+
+key = random.choice(list(quotes))
+print(key)
